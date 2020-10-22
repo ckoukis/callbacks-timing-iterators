5ea6754003f727690cdbe1c9e9706ba3dff4d69b
Christos Koukis <ckoukis@Christoss-Mac-mini.local>
2020-10-22 07:54:56 -0600

    [hello.js] Add sayHello 5times

---
+++ hello.js
@@ -0,0 +1,35 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
