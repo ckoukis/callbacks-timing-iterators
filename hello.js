c70ca4bc3da4564b6c06ef73cd886984dd864623
Christos Koukis <ckoukis@Christoss-Mac-mini.local>
2020-10-22 08:02:07 -0600

    [js] Add sayHello 10 times and checkNumber 5 times

--- hello.js
+++ hello.js
@@ -33,3 +33,37 @@ function sayHello(name) {
 
 console.log(sayHello('SEI 1019'));
 
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Chris'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Jim'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('John'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Jane'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Sally'));
